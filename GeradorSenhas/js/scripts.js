e7031313215b17283c25493fa62a84bbd4ab5c54
JuniorMelo <juniormelo.dev@gmail.com>
2022-12-25 21:54:47 -0300

    Finalização dos eventos e criação das senhas

--- GeradorSenhas/js/scripts.js
+++ GeradorSenhas/js/scripts.js
@@ -21,7 +21,40 @@ const getSymbol = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "" //inicia vazio
+
+    const passwordLength = 10 //tamanho fixo da senha gerada
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    ]
+
+    for (i = 0; i < passwordLength; i = i + 4) {
+         generators.forEach(() => {
+            const randomValue =
+                generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+         });      
+    }
+
+    password = password.slice(0, passwordLength);
+    
+    generatedPasswordElement.style.display = "block"
+    generatedPasswordElement.querySelector("h4").innerText = password;
+
+};
+
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-    console.log("teste");
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    );
 });
